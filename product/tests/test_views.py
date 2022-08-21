eeabb3710d01e598c8031f8b9e7bea1641dfad6a
shahdin-sh <99738074+shahdin-sh@users.noreply.github.com>
2022-08-21 11:43:38 +0430

    add few tests in test_views.py

--- product/tests/test_views.py
+++ product/tests/test_views.py
@@ -1,12 +1,16 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from ..models import UserComments, Product
+from accounts.models import CustomUserModel
 
+
+# the most complicated tests, train then come back and add changes
 
 class TestView(TestCase):
 
     def setUp(self):
         self.client = Client
+        self.user1 = CustomUserModel.objects.create(username='user1', profile_avatar='/media/default/img_avatar.png')
         self.product_1 = Product.objects.create(
             product_title='product1',
             product_description='random_description',
@@ -18,6 +22,17 @@ class TestView(TestCase):
         )
         self.products_list_view = reverse('products_list_view')
         self.products_detail_view = reverse('product_detail_view', args=[self.product_1.id])
+        self.user_comment_1 = UserComments.objects.create(
+            text='text1',
+            product=self.product_1,
+            user=self.user1,
+            datetime_created='some_date',
+            datetime_modified='some_modified_date',
+            is_active='True',
+            rate='good',
+        )
+        self.edit_comment = reverse('edit_user_comments', args=[self.user_comment_1.id, self.user_comment_1.product.id])
+        self.delete_comment = reverse('delete_user_comments', args=[self.user_comment_1.id, self.user_comment_1.product.id])
 
     def test_product_list_view_GET(self):
         client = Client()
@@ -40,3 +55,16 @@ class TestView(TestCase):
         client = Client()
         response = client.get(self.products_detail_view)
         self.assertContains(response, self.product_1.id)
+
+    # def test_post_detail_view_Post_add_user_comment(self):
+    #     response = self.client.post(self.products_detail_view, {
+    #         'user': self.user_comment_1.user,
+    #         'product': self.user_comment_1.product
+    #     })
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertEqual(self.user_comment_1.text, 'test1')
+
+    # def test_product_detail_view_Post_no_data_for_comments(self):
+    #     response = self.client.post(self.products_detail_view)
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertEqual(self.user_comment_1.count(), 0)
