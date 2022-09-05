4d71086077b20d800f7cfa248ca5a6d70e140bfd
shahdin-sh <99738074+shahdin-sh@users.noreply.github.com>
2022-09-05 18:28:09 +0430

    add and set up jalali dattime package

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # jalali date
+    'jalali_date',
     # custom apps
     'accounts',
     'pages',
